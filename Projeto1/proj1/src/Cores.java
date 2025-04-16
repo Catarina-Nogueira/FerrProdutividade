2071dfdae041663ef8794d368fca0b611db7017b
Catarina-Nogueira <anaacatarina.cn@gmail.com>
2025-04-16 17:08:15 +0100

    Criação Cores

---
+++ Projeto1/proj1/src/Cores.java
@@ -0,0 +1,8 @@
+public interface Cores {
+    String AZUL = "azul";
+    String AMARELO = "amarelo";
+    String VERMELHO = "vermelho";
+    String BRANCO = "branco";
+    String PRETO = "preto";
+    String CINZENTO = "cinzento";
+}
