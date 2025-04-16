30e4b0d3fe2bf160917bf098629f808b9d96d366
Catarina-Nogueira <anaacatarina.cn@gmail.com>
2025-04-16 17:07:04 +0100

    MainTributaveis

--- Projeto1/proj1/src/Main.java
+++
@@ -1,7 +0,0 @@
-//Este exercício equivale ao exercício 1 da ficha 5 de programação
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ Projeto1/proj1/src/MainTributaveis.java
@@ -0,0 +1,9 @@
+//Este exercício equivale ao exercício 1 da ficha 5 de programação
+
+public class MainTributaveis {
+    public static void main(String[] args) {
+
+        Veiculo v1 = new Veiculo("22-33-CC", 1000, "verde");
+        System.out.println("\n" + v1);
+    }
+}
