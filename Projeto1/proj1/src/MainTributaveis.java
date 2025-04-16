a29fc84a0a355cd196acd968ceb94fa1ef9c4471
Catarina-Nogueira <anaacatarina.cn@gmail.com>
2025-04-16 17:08:56 +0100

    instância v2

--- Projeto1/proj1/src/MainTributaveis.java
+++ Projeto1/proj1/src/MainTributaveis.java
@@ -5,5 +5,8 @@ public class MainTributaveis {
 
         Veiculo v1 = new Veiculo("22-33-CC", 1000, "verde");
         System.out.println("\n" + v1);
+
+        Veiculo v2 = new Veiculo("44-55-DD", 2500, Cores.AZUL);
+        System.out.println("\n" + v2);
     }
 }
